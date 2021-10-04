ef00c3f38588e26186e349848819418834288f4c
Daniel Louie <Daniel.louie27@gmail.com>
2021-10-04 10:00:30 -0700

    Variables, String, Numbers Exercises

---
+++ project0/firstName.py
@@ -0,0 +1,6 @@
+fName = "daniel"
+print(fName)
+print(fName.lower())
+print(fName.title())
+print(fName.upper())
+
---
+++ project0/oneTwo.py
@@ -0,0 +1,4 @@
+message = "learning to code in python"
+print(message)
+message = "I've learned something"
+print(message)
