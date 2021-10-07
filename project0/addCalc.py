5dc53a2101ad31e7c282eeb93bb71a336b2223c7
Daniel Louie <Daniel.louie27@gmail.com>
2021-10-07 12:58:03 -0700

    Function Exercises

---
+++ project0/addCalc.py
@@ -0,0 +1,10 @@
+# Addition Calculator¶
+# Write a function that takes in two numbers, and adds them together. Make your function print out a sentence showing the two numbers, and the result.
+# Call your function with three different sets of numbers.
+
+def addCalc(firstNum, secNum):
+    print(str(firstNum) + " + " + str(secNum) + " = " + str(firstNum+secNum))
+
+addCalc(1,2)
+addCalc(2,3)
+addCalc(17,23)
