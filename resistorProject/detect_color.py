12bf71a1134503d5f1d7cbb144313197cde34465
Daniel Louie <Daniel.louie27@gmail.com>
2022-04-21 15:02:05 -0700

    experiments with creating multiple masks to overlay

--- resistorProject/detect_color.py
+++ resistorProject/detect_color.py
@@ -1,7 +1,16 @@
+"""
+Detect colors in still image
+
+@author: Daniel Louie
+@version: 4/20/22
+Resources:
+- openCV color detection tutorial: https://pyimagesearch.com/2014/08/04/opencv-python-color-detection/
+"""
 # import the necessary packages
 import numpy as np
 import argparse
 import cv2
+
 
 # construct the argument parse and parse the arguments
 ap = argparse.ArgumentParser()
@@ -9,27 +18,37 @@ ap.add_argument("-i", "--image", help="path to the image")
 args = vars(ap.parse_args())
 # load the image
 image = cv2.imread(args["image"])
+#convert image to hsv/hsb
+image_hsv = cv2.cvtColor(image, cv2.COLOR_BGR2HSV)
 
-# define the list of boundaries
+
+
+# define the list of boundaries *(in rgba)
 # 1. Yellow rgba(253,203,0,255)
 #       *ran into an error with the boundaries of the yellow color,
 #        had to play with the bounds
 # 2. Purple rgba(159,0,152,255)
 # 3. Red rgba(244,70,0,255)
 # 4. Resistor rgba(218,188,123,255)
-# Boundaries in BGR
+# Boundaries *(in BGR)
 boundaries = [
     ([0, 195, 245], [10, 205, 255]),
     ([150, 0, 150], [160, 10, 160]),
     ([0, 65, 240], [10, 75, 250]),
     ([120, 180, 210], [130, 190, 220])
 ]
+# template for converting boundaries to a dictionary
+colors = {1: 'Geeks', 2: 'For', 3: 'Geeks'}
 
 # loop over the boundaries
 for (lower, upper) in boundaries:
     # create NumPy arrays from the boundaries
     lower = np.array(lower, dtype="uint8")
+    #test
+    print(lower)
     upper = np.array(upper, dtype="uint8")
+    #test
+    print(upper)
     # find the colors within the specified boundaries and apply
     # the mask
     mask = cv2.inRange(image, lower, upper)
@@ -37,3 +56,22 @@ for (lower, upper) in boundaries:
     # show the images
     cv2.imshow("images", np.hstack([image, output]))
     cv2.waitKey(0)
+
+def make_mask(image,lower,upper)
+    # create NumPy arrays from the boundaries
+    lower = np.array(lower, dtype="uint8")
+    # test
+    print(lower)
+    upper = np.array(upper, dtype="uint8")
+    # test
+    print(upper)
+    # find the colors within the specified boundaries and apply
+    # the mask
+    mask = cv2.inRange(image, lower, upper)
+    return mask
+
+yellow_mask = make_mask(image, boundaries[0][0],boundaries[0][1])
+purple_mask = make_mask(image, boundaries[1][0],boundaries[1][1])
+
+
+
