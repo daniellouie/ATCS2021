b82b35052b41c02cdb161005c10b1a3f5a491c85
Daniel Louie <Daniel.louie27@gmail.com>
2022-05-02 00:06:02 -0700

    adds color palette of resistorImages and improves pixel scanning loop

--- resistorProject/detect_color.py
+++ resistorProject/detect_color.py
@@ -18,18 +18,45 @@ colors = {'yellow': [[0, 195, 245], [10, 205, 255]],
           'purple': [[150, 0, 150], [160, 10, 160]],
           'red': [[0, 65, 240], [10, 75, 250]],
           'resistor': [[120, 180, 210], [130, 190, 220]]}
+# **remake colorpalette in RGB
+
+# new color palette: uses colors from ResistorColorTable.png (from: https://resistorcolorcodecalc.com/)
+# each color was tested with a color identifier website and manually run through my program to set intervals
+# set a +/- 10 for each of the R, G, B values (unless it hits 0 or 255 before then)
+resistorColors = {'resistorWire': [[99, 123, 135], [119, 143, 155]],
+                  'resistorDarkTan': [[190, 180, 119], [210, 190, 139]],
+                  'resistorDarkTan': [[219, 186, 142], [239, 206, 162]],
+                  'black': [[0, 0, 0], [10, 10, 10]],
+                  'brown': [[107, 0, 0], [127, 10, 10]],
+                  'red': [[245, 0, 0], [255, 10, 10]],
+                  'orange': [[245, 143, 0], [255, 163, 10]],
+                  'yellow': [[245, 245, 0], [255, 255, 10]],
+                  'green': [[0, 157, 70], [10, 177, 90]],
+                  'blue': [[0, 91, 174], [10, 111, 194]],
+                  'violet': [[132, 0, 245], [152, 10, 255]],
+                  'gray': [[146, 146, 146], [166, 166, 166]],
+                  'white': [[245, 245, 245], [255, 255, 255]],
+                  'gold': [[245, 200, 0], [255, 220, 10]],
+                  'silver': [[206, 206, 206], [226, 226, 226]],
+                  'pink': [[245, 206, 245], [255, 226, 255]],
+                  }
+
 # rectangle dimensions
-rect_start_x = 150
-rect_start_y = 245
-rect_end_x = 425
-rect_end_y = 260
+rect_start_x = 440
+rect_start_y = 170
+rect_end_x = 850
+rect_end_y = 190
 rect_midpoint_y = int((rect_end_y + rect_start_y)/2)
-# construct argument parse and parse the argument through terminal
-ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", help="path to the image")
-args = vars(ap.parse_args())
-# load image and image shapes
-image = cv2.imread(args["image"])
+
+# *Currently not using terminal to load the images
+# # construct argument parse and parse the argument through terminal
+# ap = argparse.ArgumentParser()
+# ap.add_argument("-i", "--image", help="path to the image")
+# args = vars(ap.parse_args())
+# # load image and image shapes
+# image = cv2.imread(args["image"])
+
+image = cv2.imread("resistorImages/10ohm_10%tolerance.png")
 height, width, depth = image.shape
 # draw rectangle to visualize scanning bar
 start_point = (rect_start_x, rect_start_y)
@@ -42,14 +69,35 @@ cv2.imshow("image", image)
 # radius = 10
 # circle = cv2.circle(image, circle_center, radius, (255, 255, 255), thickness=3, lineType=cv2.LINE_AA)
 # cv2.imshow("images", image)
+
+print("Press any key to scan pixels")
 cv2.waitKey(0)
 
+
+previousPixel = None
+currentPixel = None
+nextPixel = None
+print("Starting scan... (press 'q' to quit)")
 # loop through pixels and access RGB info
-for x in range(rect_start_x, rect_end_x):
+# shrinking range by 2 on each side so it doesn't count the pixels of the bounding box
+for x in range(rect_start_x+2, rect_end_x-2):
     y = rect_midpoint_y
-    print("x:", x, "y:", y)
-    print(image[y][x])
+    # print("x:", x, "y:", y)
+    # get the values of the current pixel
+    b,g,r = image[y][x]
+    currentPixel = r,g,b
+    # get the values of the next pixel
+    b_next, g_next, r_next = image[y][x+1]
+    nextPixel = r_next, g_next, b_next
+    # if the pixel color doesnt match the previous pixel but does match the next pixel
+    # eliminates in-between color pixels and the colors picked up by the bounding box
+    if currentPixel != previousPixel and currentPixel == nextPixel:
+        # currently prints values but will eventually call function to determine its color
+        print("x:", x, "y:", y, currentPixel)
+    previousPixel = currentPixel
+
     #*ISSUE with repainting white circle
+    # need to figure out how to animate progress without interfering with the color of the pixels it scans
     # circle_center = (x, y)
     # circle = cv2.circle(image, circle_center, radius, (255, 255, 255), thickness=3, lineType=cv2.LINE_AA)
     # cv2.imshow("images", circle)
